5cf6d79392bb98e65284fbb0c29a51672fe7f237
Case Wylie <cmwylie19@defenseunicorns.com>
2024-04-04 17:08:47 -0400

    chore: phase-3
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- capabilities/admission.ts
+++ capabilities/admission.ts
@@ -1,4 +1,5 @@
 import { Capability, PeprMutateRequest, PeprValidateRequest, a } from "pepr";
+import { V1Container } from "@kubernetes/client-node";
 // import { containers } from "pepr/sdk"
 
 export const Admission = new Capability({
@@ -25,6 +26,45 @@ When(a.Pod)
     return po.Approve();
   });
 
+const containerHelper = (container: V1Container) => {
+  // set default if not there
+  container.securityContext = container.securityContext || {};
+  if (
+    container.securityContext?.runAsUser &&
+    container.securityContext?.runAsUser < 10
+  ) {
+    container.securityContext.runAsUser = 1000;
+  } else if (!container.securityContext?.runAsUser) {
+    container.securityContext.runAsUser = 655532;
+  }
+};
+When(a.Pod)
+  .IsCreatedOrUpdated()
+  .Mutate(po => {
+    if (!po.HasLabel("ignore-me")) {
+      po.Raw.spec?.containers?.forEach(container => containerHelper(container));
+
+      po.Raw.spec?.initContainers?.forEach(container =>
+        containerHelper(container),
+      );
+
+      po.Raw.spec?.ephemeralContainers?.forEach(container =>
+        containerHelper(container),
+      );
+
+      // define if not there
+      po.Raw.spec.securityContext = po.Raw.spec.securityContext || {};
+
+      if (
+        po.Raw.spec.securityContext?.runAsUser &&
+        po.Raw.spec.securityContext?.runAsUser < 10
+      ) {
+        po.Raw.spec.securityContext.runAsUser = 1000;
+      } else if (!po.Raw.spec.securityContext?.runAsUser) {
+        po.Raw.spec.securityContext.runAsUser = 655532;
+      }
+    }
+  });
 export function containers(
   request: PeprValidateRequest<a.Pod> | PeprMutateRequest<a.Pod>,
   containerType?: "containers" | "initContainers" | "ephemeralContainers",
